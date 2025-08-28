b9b4f8f17df5f9615bb3b2f7a4d35451b28dfa8b
kieugiathinh <kieugiathinh1301@gmail.com>
2025-08-28 20:24:50 +0700

    finished working on bservices

---
+++ BackgroundServices/emailservices/sendPromotionEmail.js
@@ -0,0 +1,37 @@
+import ejs from "ejs";
+import dotenv from "dotenv";
+import sendMail from "../helpers/sendMail.js";
+import Product from "../models/product.model.js";
+import User from "../models/user.model.js";
+dotenv.config();
+
+const sendPromotionEmail = async () => {
+  const users = await User.find();
+  const products = await Product.aggregate([{ $sample: { size: 5 } }]);
+
+  for (let user of users) {
+    ejs.renderFile(
+      "templates/promotion.ejs",
+      { products },
+      async (err, data) => {
+        if (err) {
+          console.log("Error rendering EJS template:", err);
+          return;
+        }
+        let messageoption = {
+          from: process.env.EMAIL,
+          to: user.email,
+          subject: "Your weekly products",
+          html: data,
+        };
+
+        try {
+          await sendMail(messageoption);
+        } catch (error) {
+          console.log(err);
+        }
+      }
+    );
+  }
+};
+export default sendPromotionEmail;
--- BackgroundServices/index.js
+++ BackgroundServices/index.js
@@ -4,6 +4,8 @@ import dbConnection from "./utils/db.js";
 import cron from "node-cron";
 import sendWelcomeEmail from "./emailservices/sendWelcomeEmail.js";
 import sendPendingOrderEmail from "./emailservices/sendPendingOrderEmail.js";
+import sendDeliveredOrderEmail from "./emailservices/sendDeliveveredOrderEmail.js";
+import sendPromotionEmail from "./emailservices/sendPromotionEmail.js";
 dotenv.config();
 const app = express();
 
@@ -14,11 +16,19 @@ const services = () => {
   cron.schedule("* * * * *", () => {});
   sendWelcomeEmail();
   sendPendingOrderEmail();
+  sendDeliveredOrderEmail();
+};
+
+const promotion = () => {
+  cron.schedule("30 5 * * 5", () => {});
+  //sending promotion email
+  sendPromotionEmail();
 };
 
 services();
+promotion();
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Backgroundservices is running on port ${PORT}`);
   dbConnection();
 });
